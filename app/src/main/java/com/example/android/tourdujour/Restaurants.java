f85737c77b211213009385e19de36bf615733555
CJ Middleton <c_middleton@hotmail.com>
2016-08-23 18:07:47 -0500

    Reformatted Code
    Whitespace

--- app/src/main/java/com/example/android/tourdujour/Restaurants.java
+++ app/src/main/java/com/example/android/tourdujour/Restaurants.java
@@ -14,15 +14,14 @@ public class Restaurants extends AppCompatActivity {
         setContentView(R.layout.destination_list);
 
         ArrayList<Destination> destinations = new ArrayList<Destination>();
-        destinations.add(new Destination ("Ben's Chili Bowl", "1213 U St NW, Washington, DC 20009-4442", R.drawable.ben));
-        destinations.add(new Destination ("Hank's Oyster Bar", "1624 Q St NW, Washington, DC 20009", R.drawable.hanks));
-        destinations.add(new Destination ("STK", "1250 Connecticut Ave NW, Washington, DC 20036", R.drawable.stk));
-        destinations.add(new Destination ("Busboys & Poets", "2021 14th St NW, Washington, DC 20009", R.drawable.busboys));
-        destinations.add(new Destination ("Capitol Brewery", "4001 Campbell Ave, Arlington, VA 22206", R.drawable.capitolbrewery));
-        destinations.add(new Destination ("La Tasca", "722 7th St NW, Washington, DC 20001", R.drawable.latasca));
-        destinations.add(new Destination ("The Cock and Bowl", "address", R.drawable.cockandbowl));
-        destinations.add(new Destination ("KRAMERBOOKS & afterwords", "address", R.drawable.kramer));
-
+        destinations.add(new Destination("Ben's Chili Bowl", "1213 U St NW, Washington, DC 20009-4442", R.drawable.ben));
+        destinations.add(new Destination("Hank's Oyster Bar", "1624 Q St NW, Washington, DC 20009", R.drawable.hanks));
+        destinations.add(new Destination("STK", "1250 Connecticut Ave NW, Washington, DC 20036", R.drawable.stk));
+        destinations.add(new Destination("Busboys & Poets", "2021 14th St NW, Washington, DC 20009", R.drawable.busboys));
+        destinations.add(new Destination("Capitol Brewery", "4001 Campbell Ave, Arlington, VA 22206", R.drawable.capitolbrewery));
+        destinations.add(new Destination("La Tasca", "722 7th St NW, Washington, DC 20001", R.drawable.latasca));
+        destinations.add(new Destination("The Cock and Bowl", "address", R.drawable.cockandbowl));
+        destinations.add(new Destination("KRAMERBOOKS & afterwords", "address", R.drawable.kramer));
 
         DestinationAdapter adapter = new DestinationAdapter(this, destinations, R.color.category_restaurants);
 
--- app/src/main/java/com/example/android/tourdujour/Shopping.java
+++ app/src/main/java/com/example/android/tourdujour/Shopping.java
@@ -14,17 +14,13 @@ public class Shopping extends AppCompatActivity {
         setContentView(R.layout.destination_list);
 
         ArrayList<Destination> destinations = new ArrayList<Destination>();
-        destinations.add(new Destination ("Pentagon City Mall", "1100 S Hayes St, Arlington, VA 22202", R.drawable.pentagoncity));
+        destinations.add(new Destination("Pentagon City Mall", "1100 S Hayes St, Arlington, VA 22202", R.drawable.pentagoncity));
         destinations.add(new Destination("Potomac Mills Mall", "2700 Potomac Mills Cir, Woodbridge, VA 22192", R.drawable.potomacmills));
-        destinations.add(new Destination ("Tyson's Galleria", "2001 International Dr, McLean, VA 22102", R.drawable.tysons));
-        destinations.add(new Destination ("Union Station", "50 Massachusetts Ave NE, Washington, DC 20002", R.drawable.unionstation));
-        destinations.add(new Destination ("Chevy Chase Pavilion", "5335 Wisconsin Ave NW, Washington, DC 20015", R.drawable.chevychase));
-        destinations.add(new Destination ("City Center DC", "825 10th St NW, Washington, DC 20001", R.drawable.citycenter));
-        destinations.add(new Destination ("Eastern Market", "225 7th St SE, Washington, DC 20003", R.drawable.easternmarket));
-
-
-
-
+        destinations.add(new Destination("Tyson's Galleria", "2001 International Dr, McLean, VA 22102", R.drawable.tysons));
+        destinations.add(new Destination("Union Station", "50 Massachusetts Ave NE, Washington, DC 20002", R.drawable.unionstation));
+        destinations.add(new Destination("Chevy Chase Pavilion", "5335 Wisconsin Ave NW, Washington, DC 20015", R.drawable.chevychase));
+        destinations.add(new Destination("City Center DC", "825 10th St NW, Washington, DC 20001", R.drawable.citycenter));
+        destinations.add(new Destination("Eastern Market", "225 7th St SE, Washington, DC 20003", R.drawable.easternmarket));
 
         DestinationAdapter adapter = new DestinationAdapter(this, destinations, R.color.category_shopping);
 
